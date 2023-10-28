87a020580d6d3f378709aa5a14b0e205025407da
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2023-10-29 00:39:22 +0600

    footer

--- src/components/Appoinment/AvailableAppoinment.js
+++ src/components/Appoinment/AvailableAppoinment.js
@@ -1,7 +1,16 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AvailableAppoinment = ({date}) => {
+
+     const [services, setServices] = useState([])
+
+     useEffect(()=>{
+
+          fetch(`services.json`)
+          .then(res => res.json())
+          .then(data=> setServices(data))
+     },[])
      return (
           <div>
                <p className='text-center py-8 text-4xl font-bold text-teal-400'>You have date : {format(date,'PP')}</p>
