75ac49e39eb26b7e9e68c87ba2781f61e460a26a
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2023-11-20 22:15:01 +0600

    auth instally

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,64 +13,97 @@ const Login = () => {
   } = useForm();
 
   const onSubmit = (data) => {
-     
-     console.log(data)
-
-
-
-}
-
-
-
+    console.log(data);
+  };
 
   if (user) {
     console.log(user);
   }
 
-
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-slate-800">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="  text-center text-2xl text-blue-600 font-bold">
             LOG IN
           </h2>
-          <div className="form-control w-full max-w-xs">
-  <label className="label">
-    <span className="label-text">Email</span>
-    
-  </label>
-  <input 
-  {...register("email", {
-     pattern: {
-       value: /[A-Za-z]{3}/,
-       message: 'error message' // JS only: <p>error message</p> TS only support string
-     }
-   })}
-  
-  type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-  <label className="label">
-    <span className="label-text-alt">Bottom Left label</span>
-  
-  </label>
-</div>
-
-
-
-
-
 
           <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <input  />
-      <label>Gender Selection</label>
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <input type="submit" />
-    </form>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is requried",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Provide a valid email", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500" role="alert">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500" role="alert">
+                    {errors.email.message}
+                  </span>
+                )}
+                {/* <span>Bottom Left label</span> */}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is requried",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 charater",
+                  },
+                })}
+                type="password"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500" role="alert">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500" role="alert">
+                    {errors.password.message}
+                  </span>
+                )}
+                {/* <span>Bottom Left label</span> */}
+              </label>
+            </div>
+
+            <div className="card-actions justify-center py-5 ">
+              <input
+                value="Login"
+                className="btn w-full   max-w-xs  text-white btn-sm  bg-blue-900 justify-center"
+                type="submit"
+              />
+            </div>
+          </form>
           <div className="divider">OR</div>
           <div className="card-actions justify-center">
             <button
