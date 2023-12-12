fe82e755168bdecf5bd3735a9382a3c6469640b8
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2023-12-13 01:03:43 +0600

    form post api

--- src/components/Appoinment/AvailableAppoinment.js
+++ src/components/Appoinment/AvailableAppoinment.js
@@ -6,9 +6,9 @@ import BookingModal from "./BookingModal";
 const AvailableAppoinment = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment,setTreatment] = useState(null);
-
+  const formatedDate = format(date, "PP");
   useEffect(() => {
-    fetch(`http://localhost:5000/service`)
+    fetch(`http://localhost:5000/availableservices?date${formatedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
