2270d0f02d840f1b2618d4f85b2e13c8bcb3317c
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-02-10 23:40:56 +0600

    doctorsr

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -16,26 +16,10 @@ import ENT from "../../assets/images/ENT.png";
 import Gastroenterology from "../../assets/images/Gastroenterology.png";
 import Rehabilitation from "../../assets/images/rehabilitation.png";
 import Service from "./Service";
-import gsap from 'gsap';
- 
-import "./Services.css"
+import gsap from "gsap";
+
+import "./Services.css";
 const Services = () => {
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   const services = [
     {
       _id: 1,
@@ -130,35 +114,36 @@ const Services = () => {
   ];
   return (
     <div className="bg-gradient-to-r from-[#157A90] via-[#160a2c] to-[#157A90] text-black ">
-      <div className=""  >
+      <div className="">
         <section
           id="features"
           class="container mx-auto px-4 space-y-6   py-8 dark:bg-transparent md:py-12 lg:py-20"
         >
           <div class="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-            <h2 class="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl text-white"
-             data-aos="fade-up"
-             data-aos-offset="200"
-             data-aos-delay="50"
-             data-aos-duration="1000"
-             data-aos-easing="ease-in-out"
-             data-aos-mirror="true"
-             data-aos-once="false"
-            
+            <h2
+              class="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl text-white"
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              data-aos-mirror="true"
+              data-aos-once="false"
             >
               Our Department
             </h2>
           </div>
-         
-          <div id="smooth-wrapper"  >
 
-          <div id="smooth-content "   class="scroll-container  mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
-            {services.map((service) => (
-              <Service  service={service} key={service._id}   ></Service>
-            ))}
+          <div id="smooth-wrapper">
+            <div
+              id="smooth-content "
+              class="scroll-container  mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3"
+            >
+              {services.map((service) => (
+                <Service service={service} key={service._id}></Service>
+              ))}
+            </div>
           </div>
-          </div>
-
         </section>
       </div>
     </div>
