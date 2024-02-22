70ebd95dd99b24a6d2556edc76340614ec7eed2c
Sadnan-2019 <sadnan.wow@gmail.com>
2024-02-22 17:44:21 +0600

    manag lanading

--- src/components/Dashboard/ManageDepartment.js
+++ src/components/Dashboard/ManageDepartment.js
@@ -36,7 +36,7 @@ const ManageDepartment = ( ) => {
                 <td>
                   <img
                     class="object-cover object-center h-12 rounded-full w-12"
-                    src={doctor.file}
+                    src={doctor.imageUrl}
                     alt="Woman looking front"
                   />
                 </td>
--- src/components/OurFacility/OurFacility.js
+++ src/components/OurFacility/OurFacility.js
@@ -22,77 +22,78 @@ const OurFacility = () => {
 
 
 
-          <div class="relative   px-6 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
-  <div class="absolute inset-0">
-    <div class="h-1/3   sm:h-2/3"></div>
-  </div>
-  <div class="relative mx-auto max-w-7xl">
-    
-    <div class="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
-
-      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div class="flex-shrink-0">
-          <img class="h-48 w-full object-cover" src={onehundred} alt=""/>
-        </div>
-        <div class="flex flex-1 flex-col justify-between bg-white p-6">
-          <div class="flex-1">
-            <p class="text-sm font-medium text-indigo-600">
-              <a href="#" class="hover:underline">100TK VISIT DOCTOR</a>
-            </p>
-            <a href="#" class="mt-2 block">
-              <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
-              <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-            </a>
-          </div>
-           
-        </div>
+          {/* <!-- component -->/ */}
+<section class=" grid grid-cols-1 lg:grid-cols-2 px-4 sm:px-6 lg:px-4 gap-6 py-12">
+     
+{/* <div class="    flex flex-wrap md:flex-row    shadow-lg    h-64 justify-between px-10	 lx:px-12    items-center">
+   <img class="h-full w-full     rounded-lg rounded-r-none  " src={diabetices} alt="bag"/>
+   <div class="w-full md:w-2/3 px-4 py-4   rounded-lg">
+      <div class=" ">
+         <h2 class="text-xl text-white font-medium mr-auto">FREE DIABETICES CHECK-UP </h2>
+         
       </div>
-
-      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div class="flex-shrink-0">
-          <img class="h-48 w-full object-cover" 
-          src={diabetices} alt=""/>
-        </div>
-        <div class="flex flex-1 flex-col justify-between bg-white p-6">
-          <div class="flex-1">
-            <p class="text-sm font-medium text-indigo-600">
-              <a href="#" class="hover:underline">Qualityful Service</a>
-            </p>
-            <a href="#" class="mt-2 block">
-              <p class="text-xl font-semibold text-gray-900">How to use search engine optimization to drive sales</p>
-              <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-                facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi.,
-                tempore temporibus quo laudantium.</p>
-            </a>
-          </div>
-        
-        </div>
-      </div>
-
-      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div class="flex-shrink-0">
-          <img class="h-48 w-full object-cover" src={pragnet} alt=""/>
-        </div>
-        <div class="flex flex-1 flex-col justify-between bg-white p-6">
-          <div class="flex-1">
-            <p class="text-sm font-medium text-indigo-600">
-              <a href="#" class="hover:underline"></a>
-            </p>
-            <a href="#" class="mt-2 block">
-              <p class="text-xl font-semibold text-gray-900">Improve your customer experience</p>
-              <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-                harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis
-                perferendis hic.</p>
-            </a>
-          </div>
-        
-        </div>
-      </div>
-
-    </div>
-  </div>
+      <p class="text-sm text-white mt-4">
+         Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequ adipisicing elit. Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequi.
+      </p>
+     
+   </div>
 </div>
+<div class="  flex   flex-col md:flex-row    shadow-lg    h-64 justify-between px-10	 lx:px-12    items-center">
+   <img class="h-full w-full     rounded-lg rounded-r-none  " src={onehundred} alt="bag"/>
+   <div class="w-full md:w-2/3 px-4 py-4   rounded-lg">
+      <div class=" ">
+         <h2 class="text-xl text-white font-medium mr-auto">100 TK DOCOTR </h2>
+         
+      </div>
+      <p class="text-sm text-white mt-4">
+         Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequ adipisicing elit. Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequi.
+      </p>
+     
+   </div>
+</div>
+<div class="  flex   flex-col md:flex-row    shadow-lg    h-64 justify-between px-10	 lx:px-12    items-center">
+   <img class="h-full w-full     rounded-lg rounded-r-none  " src={pragnet} alt="bag"/>
+   <div class="w-full md:w-2/3 px-4 py-4   rounded-lg">
+      <div class=" ">
+         <h2 class="text-xl text-white font-medium mr-auto">SURGURY </h2>
+         
+      </div>
+      <p class="text-sm text-white mt-4">
+         Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequ adipisicing elit. Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequi.
+      </p>
+     
+   </div>
+</div> */}
+<section class="text-gray-400   ">
+    <div class="  ">
+      <div class="lg:w-full   flex flex-wrap">
+        <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64   rounded" src={diabetices}/>
+        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+          {/* <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2> */}
+          <h1 class="text-white text-3xl title-font font-medium mb-1">FREE DIABETICES CHECK-UP</h1>
+          
+          <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+          
+        </div>
+      </div>
+    </div>
+  </section>
+<section class="text-gray-400   ">
+    <div class="  ">
+      <div class="lg:w-full   flex flex-wrap">
+        <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64   rounded" src={onehundred}/>
+        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+          {/* <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2> */}
+          <h1 class="text-white text-3xl title-font font-medium mb-1">100 TAKA </h1>
+          
+          <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+          
+        </div>
+      </div>
+    </div>
+  </section>
+ 
+</section>
         </div>
     );
 };
