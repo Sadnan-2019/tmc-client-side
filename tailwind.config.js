02a1a1d69ed261e1cedbb9cc8344fcb2c9e809aa
Sadnan-2019 <sadnan.wow@gmail.com>
2025-01-15 16:50:24 +0600

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3346d3",
+          secondary: "#0287D0",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 
 }
